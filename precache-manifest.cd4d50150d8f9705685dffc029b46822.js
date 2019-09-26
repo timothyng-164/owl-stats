88061db07fcc2f672e317dd0afc2f09187b58f2d
timothyng-164 <timothyng164@csu.fullerton.edu>
2019-09-25 22:51:01 -0700

    Updates

---
+++ precache-manifest.cd4d50150d8f9705685dffc029b46822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ea9fe63bd2d5d1b9c8b188e63bc7cc",
+    "url": "/owl-stats/index.html"
+  },
+  {
+    "revision": "528ae9ed72d79f914957",
+    "url": "/owl-stats/static/css/main.b997012b.chunk.css"
+  },
+  {
+    "revision": "1a815ce7c8d9685f3de2",
+    "url": "/owl-stats/static/js/2.d5245821.chunk.js"
+  },
+  {
+    "revision": "528ae9ed72d79f914957",
+    "url": "/owl-stats/static/js/main.4435e653.chunk.js"
+  },
+  {
+    "revision": "0fcb339f7958a58eb8b5",
+    "url": "/owl-stats/static/js/runtime-main.322f72ca.js"
+  },
+  {
+    "revision": "067cc925837bb01e097cc0509a5faae2",
+    "url": "/owl-stats/static/media/owl_banner.067cc925.jpg"
+  }
+]);
